98a0fd77e1b59c58e6fca782e5e9350f0308dfa7
muhtarbekovnurtilek-dot <muhtarbekovnurtilek@gmail.com>
2026-07-19 03:30:39 +0600

    Refactor Telegram bot commands and handlers

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import telebot
+from telebot import types
 
-# 1. Создаем бота (замени ТЕКСТ_В_КАВЫЧКАХ на свой токен)
-bot = telebot.TeleBot("8469117365:AAHBuPCMromQLJoEuagEZ9es5TQQ7B6koYs")
+TOKEN = "8469117365:AAHBuPCMromQLJoEuagEZ9es5TQQ7B6koYs"
 
-# 2. Пример обработчика команды /start
+bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой новый бот NurtilekShop_bot.")
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
-# 3. Пример обработчика простого текста
+    btn1 = types.KeyboardButton("💎 Free Fire")
+    btn2 = types.KeyboardButton("🪖 PUBG Mobile")
+    btn3 = types.KeyboardButton("⭐ Telegram Stars")
+    btn4 = types.KeyboardButton("📞 Заказать")
+
+    markup.add(btn1, btn2)
+    markup.add(btn3, btn4)
+
+    bot.send_message(
+        message.chat.id,
+        "🔥 Добро пожаловать в Nurtilek Shop 💎\n\n"
+        "🎮 Донат в игры\n"
+        "⚡ Быстро и безопасно\n"
+        "💬 Поддержка: @nurt1lek_ff",
+        reply_markup=markup
+    )
+
+
 @bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, f"Вы сказали: {message.text}")
+def buttons(message):
 
-# 4. Эта строчка ОБЯЗАТЕЛЬНО должна быть в самом конце файла
-if __name__ == "__main__":
-    print("Бот запущен...")
-    bot.infinity_polling()
+    if message.text == "💎 Free Fire":
+        bot.send_message(message.chat.id, "💎 Free Fire донат\n\nПрайс скоро добавим")
+
+    elif message.text == "🪖 PUBG Mobile":
+        bot.send_message(message.chat.id, "🪖 PUBG Mobile донат\n\nПрайс скоро добавим")
+
+    elif message.text == "⭐ Telegram Stars":
+        bot.send_message(message.chat.id, "⭐ Telegram Stars\n\nПрайс скоро добавим")
+
+    elif message.text == "📞 Заказать":
+        bot.send_message(
+            message.chat.id,
+            "📩 Для заказа пиши сюда:\n@nurt1lek_ff"
+        )
+
+
+print("Бот запущен...")
+bot.infinity_polling()
